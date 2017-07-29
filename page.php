e3b654ee887c1ab2d45ee6e9e3b6b9942b04de25
Vigneshwar Sridharan <viky.viky884@gmail.com>
2017-07-29 17:46:16 +0530

    theme

--- page.php
+++ page.php
@@ -15,26 +15,43 @@
 get_header(); ?>
 
 
-			<?php
-			while ( have_posts() ) : the_post();
-                
-                if (  ! is_front_page() ) : ?>
-				    <header id="bg-head" style="background-image: url(<?php echo wp_get_attachment_image_src(8,'full',true)[0]; ?>)">
-					    <h1 class=""><i><?php single_post_title(); ?></i></h1>
-				    </header>
+		<?php
+		while ( have_posts() ) : the_post();
 
+            if (  ! is_front_page() ) : ?>
+				    <header id="bg-head" class="section-wrapper tb-pad">
+				        <div class="section-bg bottom" style="background-image: url(<?php echo wp_get_attachment_image_src(8,'full',true)[0]; ?>)">
+				            <div class="container">
+                                <div class="content">
+                                    <div class="nav-btn">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                    
+                                </div>
+				            </div>
+				        </div>
+				        <div class="container bg-white page-title">
+				            <h1 class=""><?php single_post_title(); ?></h1>
+				            <h4 class="ubuntu no-margin-bottom"><i><?php robokarthikeyan_posted_on(); ?></i></h4>
+				        </div>
+				    </header>
 			     <?php
 			     endif;
 
-				get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', get_post_format() );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			the_post_navigation();
 
-			endwhile; // End of the loop.
-			?>
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
 
 <?php
 get_sidebar();
